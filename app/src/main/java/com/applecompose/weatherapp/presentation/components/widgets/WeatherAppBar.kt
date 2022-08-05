3c32bb9d251a26a51acf51d8511a60fc7d269a6e
Jerry applegarth <applegarth1630@gmail.com>
2022-08-05 16:41:30 -0700

    1.3 added top app bar on main screen

---
+++ app/src/main/java/com/applecompose/weatherapp/presentation/components/widgets/WeatherAppBar.kt
@@ -0,0 +1,79 @@
+package com.applecompose.weatherapp.presentation.components.widgets
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.SnackbarDefaults.backgroundColor
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.rounded.MoreVert
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.input.key.Key.Companion.I
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.applecompose.weatherapp.R
+
+@Composable
+fun WeatherAppBar(
+	title: String,
+	icon: ImageVector? = null,
+	isMainScreen: Boolean = true,
+	elevation: Dp = 0.dp,
+	navController: NavController,
+	onAddActionClicked: () -> Unit = {},
+	onButtonClicked: () -> Unit = {},
+
+	) {
+	TopAppBar(
+		title = {
+			Text(
+				text = title,
+				color = MaterialTheme.colors.onPrimary,
+				style = TextStyle(
+					fontWeight = FontWeight.Bold,
+					fontSize = 20.sp
+					)
+			)
+		},
+		actions = {
+			if (isMainScreen) {
+				IconButton(onClick = { /*TODO*/ }) {
+					Icon(
+						imageVector = Icons.Default.Search,
+						contentDescription = stringResource(R.string.search_icon),
+					)
+				}
+				Icon(
+					imageVector = Icons.Rounded.MoreVert,
+					contentDescription = stringResource(R.string.moore_icon),
+				)
+			} else Box {}
+		},
+		navigationIcon = {
+						 if (icon != null) {
+							 Icon(
+								 imageVector = Icons.Default.ArrowBack,
+								 contentDescription = stringResource(R.string.back_arrow_icon),
+							    tint = MaterialTheme.colors.onPrimary,
+								 modifier = Modifier
+									 .clickable {
+										 onButtonClicked.invoke()
+									 }
+								 )
+						 }
+		},
+		backgroundColor = MaterialTheme.colors.primary,
+		elevation = elevation
+	)
+
+
+}
--- app/src/main/java/com/applecompose/weatherapp/presentation/screens/main/MainScreen.kt
+++ app/src/main/java/com/applecompose/weatherapp/presentation/screens/main/MainScreen.kt
@@ -1,65 +1,68 @@
 package com.applecompose.weatherapp.presentation.screens.main
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import android.content.ContentValues.TAG
+import android.util.Log
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.applecompose.weatherapp.data.DataOrException
 import com.applecompose.weatherapp.data.model.Weather
+import com.applecompose.weatherapp.presentation.components.widgets.WeatherAppBar
 
 @Composable
 fun MainScreen(navController: NavController, mainViewModel: MainViewModel) {
-
-	Column(
-				modifier = Modifier
-					.fillMaxSize(),
-				horizontalAlignment = Alignment.CenterHorizontally,
-				verticalArrangement = Arrangement.Top
-				)
-
-			{
-				TopAppBar(modifier = Modifier.fillMaxWidth()) {
-					Text(text = "Top App Bar",
-						fontSize = 16.sp
-						)
-
-				}
-				Text(
-					text = "Gold Prices",
-					fontSize = 24.sp,
-					)
-				Spacer(modifier = Modifier.height(16.dp))
-				Divider(thickness = 2.dp, color = MaterialTheme.colors.primaryVariant)
-				Button(
-					onClick = {  }) {
-					Text(text = "Save")
-
-				}
-				ShowData(mainViewModel)
-			}
-
-
-}
-
-@Composable
-fun ShowData(mainViewModel: MainViewModel) {
-
 	val weatherData = produceState<DataOrException<Weather, Boolean, Exception>>(
-		initialValue = DataOrException(loading = true)) {
-		value = mainViewModel.getWeatherData(42.44, -123.44)
+		initialValue = DataOrException(loading = true)
+	) {
+		value = mainViewModel.getWeatherData(42.232, -123.342)
 	}.value
 
 	if (weatherData.loading == true) {
 		CircularProgressIndicator()
 
-	}else if (weatherData.data != null) {
-		Text(text = "Main Screen: lat: ${weatherData.data!!.lat}, lon: ${weatherData.data!!.lon}")
+	} else if (weatherData.data != null) {
+		MainScaffold(weather = weatherData.data!!, navController = navController)
+	}
+}
+
+@Composable
+fun MainScaffold(weather: Weather, navController: NavController) {
+	Scaffold(
+		topBar = {
+		WeatherAppBar(
+			title = "Weather Location: ${weather.lat}, ${weather.lon}",
+
+			navController = navController,
+			elevation = 6.dp,
+			){
+
+		}
+	}
+	) {
+		MainContent(data = weather)
+	}
+}
+
+@Composable
+fun MainContent(data: Weather) {
+	Column() {
+		Text(text = data.lat.toString())
+		Text(text = data.lon.toString())
 	}
 
 
 }
+
+//Text(text = "Main Screen: lat: ${weatherData.data!!.lat}, lon: ${weatherData.data!!.lon}")
+//Spacer(modifier = Modifier.height(16.dp))
+//Text(text = "Main Screen: lat: ${weatherData.data!!}")
